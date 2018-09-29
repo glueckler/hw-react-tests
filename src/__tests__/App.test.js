13e125434cf3f87d2e9c66e4cfd82e12f9d3c485
Dean Glueckler <glueckler@gmail.com>
2018-09-29 12:51:26 -0700

    absolute paths, and beforeEach Jest function

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,14 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {shallow} from 'enzyme'
 
-import App from '../App'
-import CommentBox from '../CommentBox'
+import App from 'components/App'
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
+
+let wrapped
+beforeEach(() => {
+  wrapped = shallow(<App />)
+  
+})
 
 it('shows a comment box', () => {
-  const wrapped = shallow(<App />)
-
   // find returns an array of each instance that was found..
   // there should be exactly one in this example
   expect(wrapped.find(CommentBox).length).toEqual(1)
-  
+})
+
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1)
 })
