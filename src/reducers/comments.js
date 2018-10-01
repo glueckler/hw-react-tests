66c336c29a490e50be69cd547d374478ab367e5e
Dean Glueckler <glueckler@gmail.com>
2018-10-01 11:06:02 -0700

    more redux setup

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,5 +1,9 @@
-export default (state = {}, action) => {
+import {saveComment} from 'actions/types'
+
+export default (state = [], action) => {
   switch (action.type) {
+    case saveComment: 
+      return [...state, action.payload]
     default:
       return state
   }
